c5283e4142d1c931c9d900810e4082d1019b1a26
SaneManiacMG <mmoroane@hotmail.com>
2023-04-02 12:43:00 +0200

    Created HeroService

--- src/app/component/heroes/heroes.component.ts
+++ src/app/component/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/model/hero';
-import { HEROES } from 'src/app/model/mock-heroes';
+import { HeroService } from 'src/app/service/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +8,20 @@ import { HEROES } from 'src/app/model/mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
