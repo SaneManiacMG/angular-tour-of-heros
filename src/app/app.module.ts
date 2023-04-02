2cbb94c4a43b6b5916e6b227e0cd2b0510b02580
SaneManiacMG <mmoroane@hotmail.com>
2023-04-02 12:58:15 +0200

    Subscribe in HeroComponent Created Messages Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './component/heroes/heroes.component';
 import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
+import { MessagesComponent } from './component/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
