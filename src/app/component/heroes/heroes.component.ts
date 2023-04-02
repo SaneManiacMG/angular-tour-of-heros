f9a623581a5d57396a93f21d6ed0e6b8bbd55a64
SaneManiacMG <mmoroane@hotmail.com>
2023-04-02 17:19:00 +0200

    ...

--- src/app/component/heroes/heroes.component.ts
+++ src/app/component/heroes/heroes.component.ts
@@ -15,7 +15,7 @@ export class HeroesComponent  implements OnInit{
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id = ${hero.id}');
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
   }
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
