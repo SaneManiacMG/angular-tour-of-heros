fd31a40412b34339debd12838e576bbc459aa97e
SaneManiacMG <mmoroane@hotmail.com>
2023-04-03 07:51:04 +0200

    Added async getHero method Removed selected hero var Added a details route

--- src/app/component/heroes/heroes.component.ts
+++ src/app/component/heroes/heroes.component.ts
@@ -8,17 +8,15 @@ import { MessageService } from 'src/app/service/message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent  implements OnInit{
-  
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
+export class HeroesComponent  implements OnInit {
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  onSelect(hero: Hero): void {
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
